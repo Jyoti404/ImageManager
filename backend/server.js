676698545567bc02cf4f768b31025a5504e8dbdc
Jyoti404 <guddanmaurya2004@gmail.com>
2025-08-09 08:57:13 +0530

    cors error resolved

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Handle preflight OPTIONS requests (required for CORS)
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
 
 
 app.use(express.json());
