295940b58a5c8a0c498baa9f4fb92136c05490ad
Jyoti404 <guddanmaurya2004@gmail.com>
2025-08-09 09:08:13 +0530

    cors error resolved

--- backend/server.js
+++ backend/server.js
@@ -9,28 +9,22 @@ const imageRoutes = require('./routes/imageRoutes');
 
 const app = express();
 
-// Middleware
-
-
 const corsOptions = {
-  origin: 'https://image-manager-lft2.vercel.app', // Your frontend's origin
-  methods: ['GET', 'POST', 'OPTIONS'], // Allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  origin: 'https://image-manager-lft2.vercel.app',
+  methods: ['GET', 'POST',  "PUT", "DELETE",'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, 
 };
-// app.options('*', cors());
 app.use(cors(corsOptions));
-
-// Handle preflight OPTIONS requests (required for CORS)
-// app.options('*', cors(corsOptions));
-
+app.options('*', cors(corsOptions));
 
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
-// Ensure uploads folder exists
+
 if (!fs.existsSync('uploads')) fs.mkdirSync('uploads', { recursive: true });
 
-// Connect DB
+
 connectDB();
 
 // Routes
@@ -38,13 +32,8 @@ app.use('/api/auth', authRoutes);
 app.use('/api/folders', folderRoutes);
 app.use('/api/images', imageRoutes);
 
-// Start server
+
 const PORT = process.env.PORT || 5000;
-// console.log("Registered routes:");
-// app._router.stack.forEach(r => {
-//   if (r.route && r.route.path) {
-//     console.log(r.route.path);
-//   }
-// });
+
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
